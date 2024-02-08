35e26d118e2456ffae1dd6858bc356885b036f38
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-02-08 09:03:35 -0300

    clase 22

---
+++ Clase-22/src/Padre.js
@@ -0,0 +1,14 @@
+import Hijo from './componentes/Hijo';
+
+import './Padre.css';
+
+function App() {
+  return (
+    <div >  
+      <h1>Componente Padre</h1>
+      <Hijo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Clase-22/src/componentes/Hijo.js
@@ -0,0 +1,12 @@
+
+export default function Hijo() {
+  return (
+    <div >
+        <h2>Componente Hijo</h2>
+        <p>Informacion desde componente padre:</p>
+
+    </div>
+  );
+}
+
+
