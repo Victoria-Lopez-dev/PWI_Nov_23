bf25fd37db0c6cafb1fba71bc79b8a1588f42a03
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-02-15 09:09:07 -0300

    clase 23

---
+++ Clase-23/src/State.js
@@ -0,0 +1,13 @@
+
+import './State.css';
+
+function App() {
+  return (
+    <div className='d-flex flex-column align-items-center mt-4 gap-3'>
+    <h1>dato</h1>
+      <button className='btn btn-success'>click</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ Clase-23/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import State from './State';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <State />
+  </React.StrictMode>
+);
+
